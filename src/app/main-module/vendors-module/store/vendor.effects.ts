3eb0ee7ba50252bcd27b86de985b45b5c74313db
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-05-25 18:13:48 +0530

    removed all 'any's from the vendor module

--- src/app/main-module/vendors-module/store/vendor.effects.ts
+++ src/app/main-module/vendors-module/store/vendor.effects.ts
@@ -1,23 +1,15 @@
-import {  Injectable, inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, map, of, switchMap, tap } from "rxjs";
-import { CookieService } from "ngx-cookie-service";
-import { Store } from "@ngrx/store";
 import { VendorsService } from "../services/vendors.service";
 import { fetchVendorsRequest, fetchVendorsFailure, fetchVendorsSuccess, updateVendorRequest, updateVendorSuccess, updateVendorFailure, deleteVendorRequest, deleteVendorSuccess, deleteVendorFailure, addVendorSuccess, addVendorFailure, addVendorRequest } from "./vendor.actions";
-import { IGlobalState } from "../../../models/global";
 
 @Injectable()
 export class vendorEffects {
-    action$ = inject(Actions)
-    vendorsService$ = inject(VendorsService)
-    // appService$ = inject(AppService);
-    cs = inject(CookieService);
-    store = inject(Store<{ global: IGlobalState }>)
-
-    constructor() {
-        console.log("action$", this.action$)
-    }
+    constructor(
+        private action$: Actions,
+        private vendorsService$: VendorsService,
+    ) {}
 
     fetchVendors$ = createEffect(() =>
         this.action$.pipe(
@@ -26,7 +18,7 @@ export class vendorEffects {
                 this.vendorsService$.fetchVendors().pipe(
                     map((res: any) => {
                         console.log("res:", res)
-                        return fetchVendorsSuccess({vendors: res})
+                        return fetchVendorsSuccess({ vendors: res })
                     }),
                     catchError((err) =>
                         of(fetchVendorsFailure({ error: "Something went wrong" }))
@@ -39,7 +31,7 @@ export class vendorEffects {
     updateVendor$ = createEffect(() =>
         this.action$.pipe(
             ofType(updateVendorRequest),
-            switchMap((vendor) =>
+            switchMap(({ vendor }) =>
                 this.vendorsService$.updateVendor(vendor).pipe(
                     map((res: any) => {
                         console.log("updateVendor res:", res)
@@ -57,8 +49,8 @@ export class vendorEffects {
     deleteVendor$ = createEffect(() =>
         this.action$.pipe(
             ofType(deleteVendorRequest),
-            switchMap((data) =>
-                this.vendorsService$.deleteVendor(data._id).pipe(
+            switchMap(({ vendorId }) =>
+                this.vendorsService$.deleteVendor(vendorId).pipe(
                     map((res: any) => {
                         console.log("deleteVendor res:", res)
                         return deleteVendorSuccess({ vendor: res })
