72b3f6860b8615ce2a12ae215873d4dbbf2bee71
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-05-28 11:34:59 +0530

    fixed routing issue and landingPage flashing issue

--- src/app/guards/logout.guard.ts
+++ src/app/guards/logout.guard.ts
@@ -6,10 +6,22 @@ import { CookieService } from '../services/cookie.service';
     providedIn: 'root'
 })
 export class LogoutGuard implements CanActivate {
-    constructor(private cookieService: CookieService, private router: Router) { }
-
+    constructor(private cookieService: CookieService, private router: Router) {}
+    
+    
     canActivate(): boolean {
-        const token = this.cookieService.get('token');
+        let token;
+    
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ');
+    
+        for (let cookie of cookies) {
+          const [cookieName, cookieValue] = cookie.split('=');
+          if (cookieName === "token") {
+            token = cookieValue;
+          }
+        }
+
         if (token) {
             this.router.navigate(['/']);
             return false;
