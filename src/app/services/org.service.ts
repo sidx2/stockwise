95c92072d41e64923b7814eb2c0c922bb173cb37
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-05-30 16:38:00 +0530

    removed redundant code from the org service

--- src/app/services/org.service.ts
+++ src/app/services/org.service.ts
@@ -1,23 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BASE_URL } from '../constants/constants';
-import { CookieService } from './cookie.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrgService {
-  userId;
-  constructor(private cs: CookieService, private http: HttpClient) { 
-      try {
-        this.userId = JSON.parse(this.cs.get("user") || "{}")._id
-      }
-      catch(e) {
-        console.log(e)
-      }
-  }
+  constructor(private http: HttpClient) { }
 
-  getOrgByUserId(userId: string) {
+  getOrgByUserId() {
     return this.http.get(`${BASE_URL}/org/getOrg/`)
   }
 }
