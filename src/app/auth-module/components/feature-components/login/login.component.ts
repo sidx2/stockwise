7fccdf5362b46556c9e822376150fc6628fc45b2
Siddhesh Pardeshi <siddheshpardeshi0@gmail.com>
2024-06-07 12:36:18 +0530

    implemented concatMap in the login and signup

--- src/app/auth-module/components/feature-components/login/login.component.ts
+++ src/app/auth-module/components/feature-components/login/login.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Actions, ofType } from "@ngrx/effects";
-import { Router } from "@angular/router"
-import { Subject, takeUntil } from 'rxjs';
+import { Router } from "@angular/router";
+import { Subject, takeUntil, concatMap, timeout, catchError, of } from 'rxjs';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IAuthState, LoginCredentials } from '../../../models/auth';
 import { authStateSelector } from '../../../store/auth.selectors';
 import { ToastrService } from 'ngx-toastr';
 import { customValidators } from '../../../../shared-module/validators/customValidators';
 import { CookieService } from '../../../../services/cookie.service';
-import { fetchOrg, fetchOrgSuccess, loginUser, loginUserSuccess } from '../../../store/auth.actions';
+import { fetchOrg, fetchOrgSuccess, loginUser, loginUserSuccess, fetchOrgFailure } from '../../../store/auth.actions';
+import { User } from '../../../../models/global';
 
 @Component({
   selector: 'app-auth',
@@ -24,7 +25,7 @@ export class LoginComponent implements OnDestroy {
   loginForm = new FormGroup({
     email: new FormControl("", [Validators.required, customValidators.validEmail]),
     password: new FormControl("", [Validators.required, customValidators.strongPassword])
-  })
+  });
 
   constructor(
     private store: Store<{ auth: IAuthState }>,
@@ -33,50 +34,52 @@ export class LoginComponent implements OnDestroy {
     private actions$: Actions,
     private toastr: ToastrService,
   ) {
-
     this.store.select(authStateSelector).pipe(
       takeUntil(this.destroySubject),
     ).subscribe((authState) => {
       this.isLoading = authState.isLoading;
-    })
+    });
 
     this.actions$.pipe(
       ofType(loginUserSuccess),
+      concatMap(({ user }) => {
+        this.setLoginCookies(user, 3);
+        this.store.dispatch(fetchOrg({ userId: user._id }));
+
+        return this.actions$.pipe(
+          ofType(fetchOrgSuccess, fetchOrgFailure)
+        );
+      }),
       takeUntil(this.destroySubject)
-      
-    ).subscribe(({ user }) => {
-
-      const expiryDate = new Date();
-      expiryDate.setDate(expiryDate.getDate() + 3); // Add 3 days
-
-      // set cookies
-      this.cookieService.set("token", user.token!, 3)
-      this.cookieService.set("user", JSON.stringify(user), 3)
-      this.cookieService.set("isLoggedIn", "true", 3)
-
-      this.store.dispatch(fetchOrg({ userId: user._id }))
-    });
-
-    this.actions$.pipe(
-      ofType(fetchOrgSuccess),
-      takeUntil(this.destroySubject)
-    ).subscribe(({ org }) => {
-      this.cookieService.set("org", JSON.stringify(org), 3)
-
-      this.router.navigate(['dashboard']);
+    ).subscribe((action) => {
+      if (action && action.type === fetchOrgSuccess.type) {
+        const { org } = action;
+        this.cookieService.set("org", JSON.stringify(org), 3);
+        this.router.navigate(['dashboard']);
+      }
     });
   }
 
+  setLoginCookies(user: User, expiryInDays: number = 1) {
+    const expiryDate = new Date();
+    expiryDate.setDate(expiryDate.getDate() + expiryInDays);
+
+    // Set cookies
+    this.cookieService.set("token", user.token!, expiryInDays);
+    this.cookieService.set("user", JSON.stringify(user), expiryInDays);
+    this.cookieService.set("isLoggedIn", "true", expiryInDays);
+  }
+  
   onFormSubmit() {
     if (!this.loginForm.valid) {
-      this.toastr.error("Invalid credentails for login");
+      this.toastr.error("Invalid credentials for login");
       return;
-    };
+    }
 
     const credentials: LoginCredentials = {
       email: this.loginForm.value.email!,
       password: this.loginForm.value.password!,
-    }
+    };
 
     this.store.dispatch(loginUser({ credentials }));
   }
